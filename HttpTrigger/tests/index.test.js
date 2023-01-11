23a60efa407d7ce8657962ed34c46429158e8d8b
Vinod Ramasubbu <79949043+vinodramasubbu@users.noreply.github.com>
2023-01-11 19:23:51 +0000

    commit func code 2

--- HttpTrigger/tests/index.test.js
+++ HttpTrigger/tests/index.test.js
@@ -6,26 +6,26 @@ let expect = chai.expect;
 
 it('Http trigger should return known text', async () => {
 
-   // const request = {
-   //     query: { name: 'Bill' }
-   // };
-
-    const data = {
-            "name": "Bill"
+    const request = {
+        query: { name: 'Bill' }
     };
 
-        const url = 'https://azfnslots1-stage.azurewebsites.net/api/HttpTrigger'
+    //const data = {
+    //        "name": "Bill"
+    //};
 
-    //await httpFunction(context, request);
+    //    const url = 'https://azfnslots1-stage.azurewebsites.net/api/HttpTrigger'
+
+    await httpFunction(context, request);
     //console.log(context.log.callCount);
     //console.log(context.res.body);
     //expect(context.log.callCount).to.equal(1);
     //expect(context.res.body).to.equal('Hello Bill');
     //console.log(data);
 
-    const response = await axios.post(url,  data)
-    console.log(`statusCode: ${response.statusCode}`);
-    console.log(`responseData: ${response.data}`);
+    //const response = await axios.post(url,  data)
+    //console.log(`statusCode: ${response.statusCode}`);
+    //console.log(`responseData: ${response.data}`);
     expect(response.data).to.equal('Hello Bill');
 
 });
